08290b7c2ece8972e5e593246935d65e18f65192
giuliana2002 <92090869+giuliana2002@users.noreply.github.com>
2024-03-10 20:56:35 +0100

    finalll

--- app/src/main/java/com/example/petpetpet/Login.kt
+++ app/src/main/java/com/example/petpetpet/Login.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petpetpet.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
@@ -13,7 +14,9 @@ import com.google.firebase.database.FirebaseDatabase
 class Login : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var sharedPreferences: SharedPreferences
-    private var databaseUsuarios: DatabaseReference = FirebaseDatabase.getInstance("https://petpetpet-2460d-default-rtdb.europe-west1.firebasedatabase.app").getReference("Usuarios")
+    private var databaseUsuarios: DatabaseReference =
+        FirebaseDatabase.getInstance("https://petpetpet-2460d-default-rtdb.europe-west1.firebasedatabase.app")
+            .getReference("Usuarios")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,44 +59,60 @@ class Login : AppCompatActivity() {
                 }
             }
 
-            databaseUsuarios.child(usuarioIngresado).child(usuarioIngresado).get().addOnSuccessListener {
-                databaseUsuarios.child(usuarioIngresado).child("contrasenaval").get().addOnSuccessListener {
-                    val contrasenaReal = it.value.toString()
-                    if (contrasenaReal == contrasenaIngresada) {
-                        databaseUsuarios.child(usuarioIngresado).child("estado").get().addOnSuccessListener { dataSnapshot ->
-                            if (dataSnapshot.exists()) {
-                                val estado = dataSnapshot.value.toString()
-                                if (estado == "activo") {
-
-                                    databaseUsuarios.child(usuarioIngresado).child("tipo").get().addOnSuccessListener { dataSnapshot ->
+            databaseUsuarios.child(usuarioIngresado).child(usuarioIngresado).get()
+                .addOnSuccessListener {
+                    databaseUsuarios.child(usuarioIngresado).child("contrasenaval").get()
+                        .addOnSuccessListener {
+                            val contrasenaReal = it.value.toString()
+                            if (contrasenaReal == contrasenaIngresada) {
+                                databaseUsuarios.child(usuarioIngresado).child("estado").get()
+                                    .addOnSuccessListener { dataSnapshot ->
                                         if (dataSnapshot.exists()) {
-                                            val tipo = dataSnapshot.value.toString()
-                                            val intent = Intent(this, RegistroAnimales::class.java)
-                                            intent.putExtra("usuario", usuarioIngresado)
-                                            intent.putExtra("tipo", tipo)
-                                            startActivity(intent)
+                                            val estado = dataSnapshot.value.toString()
+                                            if (estado == "activo") {
+
+                                                databaseUsuarios.child(usuarioIngresado)
+                                                    .child("tipo").get()
+                                                    .addOnSuccessListener { dataSnapshot ->
+                                                        if (dataSnapshot.exists()) {
+                                                            val tipo = dataSnapshot.value.toString()
+                                                            val intent = Intent(
+                                                                this,
+                                                                RegistroAnimales::class.java
+                                                            )
+                                                            intent.putExtra(
+                                                                "usuario",
+                                                                usuarioIngresado
+                                                            )
+                                                            intent.putExtra("tipo", tipo)
+                                                            startActivity(intent)
+                                                            Toast.makeText(
+                                                                this@Login,
+                                                                "Conectado con Exito",
+                                                                Toast.LENGTH_SHORT
+                                                            ).show()
+                                                        } else {
+                                                            fallo()
+                                                        }
+                                                    }.addOnCanceledListener {
+                                                    fallo()
+                                                }
+                                            } else {
+                                                fallo()
+                                            }
                                         } else {
                                             fallo()
                                         }
                                     }.addOnCanceledListener {
-                                        fallo()
-                                    }
-                                } else {
                                     fallo()
                                 }
                             } else {
                                 fallo()
                             }
                         }.addOnCanceledListener {
-                            fallo()
-                        }
-                    }else {
                         fallo()
                     }
                 }.addOnCanceledListener {
-                    fallo()
-                }
-            }.addOnCanceledListener {
                 fallo()
             }
 
@@ -105,8 +124,8 @@ class Login : AppCompatActivity() {
         }
     }
 
-    private fun fallo(){
-        Snackbar.make(binding.root, "Usuario o contraseña no son correctos", Snackbar.LENGTH_SHORT).show()
+    private fun fallo() {
+        Toast.makeText(this, "Error al conectar", Toast.LENGTH_SHORT).show()
         // Limpiar los campos de texto
         binding.CuadroNombre.text?.clear()
         binding.cuadroContra.text?.clear()
@@ -114,7 +133,6 @@ class Login : AppCompatActivity() {
         // Devolver el foco al campo de usuario
         binding.CuadroNombre.requestFocus()
     }
-
 
 
 }
